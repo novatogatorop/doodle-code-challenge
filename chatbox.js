2934eab133f3e98c0b558028b514a6b6458eeb7d
nova <nova@roadbeats.com>
2020-12-17 22:11:22 +0100

    a form to add new message

--- chatbox.js
+++ chatbox.js
@@ -26,14 +26,33 @@ const messages = [
   },
 ]
 
-renderMessages();
+init();
+
+function init(){
+  renderMessages();
+  document.querySelector(".btn").onclick = addNewMessage;
+};
 
 function renderMessages(){
   document.querySelector(".messages").innerHTML = messages.map(function(message) {
     return `
-    <li>${message.username} -
-    <strong>${message.text}</strong> -
-    ${message.created_at.getDate()} ${message.created_at.toLocaleString('default', { month: 'short' })} ${message.created_at.getFullYear()} ${message.created_at.getHours()} : ${message.created_at.getMinutes()}</li>`
+    <li>
+      <p>${message.username}</p>
+      <p><strong>${message.text}</strong></p>
+      <p>${message.created_at.getDate()} ${message.created_at.toLocaleString('default', { month: 'short' })} ${message.created_at.getFullYear()} ${message.created_at.getHours()} : ${message.created_at.getMinutes()}</p>
+    </li>`
   }).join("");
+};
+
+function addNewMessage(){
+  const text = document.querySelector(".text-input").value
+
+  messages.push({
+    username: "me",
+    text: text,
+    created_at: new Date()
+  })
+
+  renderMessages();
 };
 
